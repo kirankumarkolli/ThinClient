8312dafddcff5b4cc2d15607bbfccd72fe7e9210
Sourabh Jain <sourabhjain@microsoft.com>
2022-07-27 23:18:13 +0530

    made better

--- Microsoft.Azure.Cosmos/src/Telemetry/ClientTelemetry.cs
+++ Microsoft.Azure.Cosmos/src/Telemetry/ClientTelemetry.cs
@@ -334,6 +334,8 @@ namespace Microsoft.Azure.Cosmos.Telemetry
                         request.Headers.Add(HttpConstants.HttpHeaders.EnvironmentName, envName);
                     }
 
+                    request.Headers.Add("x-ms-correlation-id", "DotNetSDK-" + Guid.NewGuid().ToString());
+
                     return request;
                 }
 
